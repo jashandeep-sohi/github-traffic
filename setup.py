4f17c2bfb512e810ad40a935a7db90eb63c5cb27
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2019-08-11 12:34:02 -0700

    specify dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version="0.1.0",
     py_modules=["github_traffic"],
     entry_points="""
-      [console_scripts]
-      github-traffic=github_traffic:cli
-    """
+        [console_scripts]
+        github-traffic=github_traffic:cli
+    """,
+    install_requires=[
+        "PyGithub>=1.43.8",
+        "click>=7",
+        "terminaltables",
+    ]
 )
